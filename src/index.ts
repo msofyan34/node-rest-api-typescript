1dec06fcfb2da7d87148ac62cb28b1a47624c69d
msofyan34 <muhammad.sofyan34@gmail.com>
2024-05-12 15:04:31 +0700

    Setup Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, {Application, Request, Response, NextFunction} from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: Application = express();
 const port: Number = 8000;
 
 app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({status: 200})
-})
+  res.status(200).send({ status: 200 });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
